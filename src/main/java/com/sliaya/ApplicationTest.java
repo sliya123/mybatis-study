0c60c41e4f46bd288a890069de934911575aacad
sliaya <2994228908@qq.com>
2023-02-24 14:07:26 +0800

    third commit

--- src/main/java/com/sliaya/ApplicationTest.java
+++ src/main/java/com/sliaya/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.sliaya;
 
 import com.sliaya.xyq.config.MyConfig;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationTest {
@@ -9,7 +10,8 @@ public class ApplicationTest {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfig.class);
-        Object dataSource = applicationContext.getBean("user");
+        SqlSessionFactoryBean sqlSessionFactoryBean = (SqlSessionFactoryBean) applicationContext.getBean("SqlSessionFactoryBean");
+        System.out.println("sqlSessionFactoryBean = " + sqlSessionFactoryBean);
 
     }
 }
